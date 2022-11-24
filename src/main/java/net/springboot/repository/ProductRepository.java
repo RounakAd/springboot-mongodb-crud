b5dc9dd4a928db4ebe632ff851348b85974536e4
Rounak Adhikary <radhikary@cinedigm.com>
2022-11-24 18:40:21 +0530

    Repository changes

--- src/main/java/net/springboot/repository/ProductRepository.java
+++ src/main/java/net/springboot/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package net.springboot.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import net.springboot.model.Product;
 
+@Repository
 public interface ProductRepository extends MongoRepository < Product, Long > {
 
 }
